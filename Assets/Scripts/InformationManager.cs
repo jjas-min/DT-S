f60b453ebd3b420652093ea9ae79762485bf2510
정승민 <21800664@handong.ac.kr>
2024-02-15 15:50:44 +0900

    read 사용량 튀는 문제 해결

--- Assets/Scripts/InformationManager.cs
+++ Assets/Scripts/InformationManager.cs
@@ -67,53 +67,15 @@ public class InformationManager : MonoBehaviour
     {
         informationPanel.SetActive(false); // X 버튼 클릭 시 Information Panel 비활성화
     }
-    public void DisplayInformation()
+    public void DisplayInformation(string information, int level, DateTime creationTime, string location)
     {
-        if (string.IsNullOrEmpty(selectedMarkerId))
-        {
-            Debug.LogError("Selected marker ID is not set.");
-            return;
-        }
+        informationText.text = $"Information: {information}";
+        levelText.text = $"Level: {level}";
+        timestampText.text = $"Created: {creationTime.ToString("yyyy-MM-dd HH:mm:ss")}";
+        locationText.text = $"Location: {location}";
 
-        // Firestore로부터 선택된 마커의 정보 조회
-        DocumentReference docRef = db.Collection("markers").Document(selectedMarkerId);
-        docRef.GetSnapshotAsync().ContinueWithOnMainThread(task =>
-        {
-            if (task.IsCompleted && !task.IsFaulted)
-            {
-                DocumentSnapshot snapshot = task.Result;
-                if (snapshot.Exists)
-                {
-                    // Firestore에서 가져온 데이터를 딕셔너리로 변환
-                    Dictionary<string, object> markerData = snapshot.ToDictionary();
-                    string information = markerData.ContainsKey("information") ? markerData["information"].ToString() : "No information";
-                    string level = markerData.ContainsKey("level") ? markerData["level"].ToString() : "No level";
-                    DateTime creationTime;
-                    if (markerData.ContainsKey("creationTime") && markerData["creationTime"] is Timestamp timestamp)
-                    {
-                        creationTime = timestamp.ToDateTime();
-                    }
-                    else
-                    {
-                        creationTime = DateTime.UtcNow; // 기본값으로 현재 시간을 사용
-                    }
-                    string location = markerData.ContainsKey("location") ? markerData["location"].ToString() : "No location";
-                    // UI 컴포넌트에 데이터 표시
-                    informationText.text = $"Information: {information}";
-                    levelText.text = $"Level: {level}";
-                    timestampText.text = $"Created: {creationTime.ToString("yyyy-MM-dd HH:mm:ss")}";
-                    locationText.text = $"location: {location}";
-                }
-                else
-                {
-                    Debug.LogError("Marker data not found.");
-                }
-            }
-            else
-            {
-                Debug.LogError("Failed to fetch marker information from Firestore.");
-            }
-        });
+        // 정보 패널 활성화
+        informationPanel.SetActive(true);
     }
 
-}
+   }
