5dc2acf8ca49fcbf240cbbe1d4bbba8f7fb1b5ef
정승민 <21800664@handong.ac.kr>
2024-02-19 16:53:20 +0900

    MarkerData streaming 구현
    MarkerData streaming 구현

--- Assets/Scripts/Marker/MarkerManager.cs
+++ Assets/Scripts/Marker/MarkerManager.cs
@@ -103,47 +103,104 @@ public class MarkerManager : MonoBehaviour
             {"location", location }
         };
 
-        db.Collection("markers").Document(markerId).SetAsync(markerDict);
+        db.Collection("Markers").Document(markerId).SetAsync(markerDict);
         selectionUI.SetActive(false);
     }
     void MarkerLoader()
     {
-        db.Collection("markers").GetSnapshotAsync().ContinueWithOnMainThread(task =>
+        FirebaseFirestore db = FirebaseFirestore.DefaultInstance;
+        db.Collection("Markers").Listen(snapshot =>
         {
-            if (task.IsCompleted && !task.IsFaulted)
+            foreach (var change in snapshot.GetChanges())
             {
-                Debug.Log("Marker data loaded successfully.");
-                foreach (DocumentSnapshot document in task.Result.Documents)
+                switch (change.ChangeType) // 여기를 수정
                 {
-                    Dictionary<string, object> markerData = document.ToDictionary();
-                    Dictionary<string, object> positionData = markerData["position"] as Dictionary<string, object>;
-                    Vector3 position = new Vector3(
-                        Convert.ToSingle(positionData["x"]),
-                        Convert.ToSingle(positionData["y"]),
-                        Convert.ToSingle(positionData["z"])
-                    );
-                    GameObject markerInstance = Instantiate(markerPrefab, position, Quaternion.identity, markerContainer.transform);
-                    markerInstance.name = document.Id;
-
-                    string information = markerData["information"].ToString();
-                    int level = int.Parse(markerData["level"].ToString());
-                    Timestamp creationTime = (Timestamp)markerData["creationTime"];
-                    string location = markerData["location"].ToString();
-
-                    MarkerData markerComponent = markerInstance.AddComponent<MarkerData>();
-                    markerComponent.id = document.Id;
-                    markerComponent.position = position;
-                    markerComponent.information = information;
-                    markerComponent.level = level;
-                    markerComponent.creationTime = creationTime.ToDateTime();
-                    markerComponent.location = location;
-
-                    MarkerClickDetector clickDetector = markerInstance.AddComponent<MarkerClickDetector>();
-                    MarkerInfoManager infoManager = FindObjectOfType<MarkerInfoManager>();
-                    clickDetector.markerInfoManager = infoManager;
+                    case DocumentChange.Type.Added:
+                        Debug.Log($"New document added: {change.Document.Id}");
+                        CreateOrUpdateMarker(change.Document);
+                        break;
+                    case DocumentChange.Type.Modified:
+                        Debug.Log($"Document modified: {change.Document.Id}");
+                        CreateOrUpdateMarker(change.Document);
+                        break;
+                    case DocumentChange.Type.Removed:
+                        Debug.Log($"Document removed: {change.Document.Id}");
+                        RemoveMarker(change.Document.Id);
+                        break;
                 }
             }
         });
     }
 
+
+    void CreateOrUpdateMarker(DocumentSnapshot document)
+    {
+        Dictionary<string, object> markerData = document.ToDictionary();
+        Dictionary<string, object> positionData = markerData["position"] as Dictionary<string, object>;
+        Vector3 position = new Vector3(
+            Convert.ToSingle(positionData["x"]),
+            Convert.ToSingle(positionData["y"]),
+            Convert.ToSingle(positionData["z"])
+        );
+        string information = markerData["information"].ToString();
+        int level = int.Parse(markerData["level"].ToString());
+        Timestamp creationTime = (Timestamp)markerData["creationTime"];
+        string location = markerData["location"].ToString();
+        // 마커가 이미 존재하는지 확인
+        GameObject existingMarker = GameObject.Find(document.Id);
+        if (existingMarker != null)
+        {
+            // 마커가 이미 존재하면 위치와 기타 데이터 업데이트
+            existingMarker.transform.position = position;
+            // 기타 데이터 업데이트 로직 추가
+            UpdateMarkerData(existingMarker, markerData);
+        }
+        else
+        {
+            // 새 마커 생성
+            GameObject markerInstance = Instantiate(markerPrefab, position, Quaternion.identity, markerContainer.transform);
+            markerInstance.name = document.Id; // GameObject의 이름을 문서 ID로 설정
+                                               // 마커 데이터 설정
+            MarkerData markerComponent = markerInstance.AddComponent<MarkerData>();
+            markerComponent.id = document.Id;
+            markerComponent.position = position;
+            markerComponent.information = information;
+            markerComponent.level = level;
+            markerComponent.creationTime = creationTime.ToDateTime();
+            markerComponent.location = location;
+
+            MarkerClickDetector clickDetector = markerInstance.AddComponent<MarkerClickDetector>();
+            MarkerInfoManager infoManager = FindObjectOfType<MarkerInfoManager>();
+            clickDetector.markerInfoManager = infoManager;
+        }
+    }
+
+    void RemoveMarker(string documentId)
+    {
+        GameObject markerToBeRemoved = GameObject.Find(documentId);
+        if (markerToBeRemoved != null)
+        {
+            Destroy(markerToBeRemoved);
+            Debug.Log($"Marker {documentId} removed successfully.");
+        }
+        else
+        {
+            Debug.LogWarning($"Marker {documentId} not found for removal.");
+        }
+    }
+   
+
+    void UpdateMarkerData(GameObject marker, Dictionary<string, object> markerData)
+    {
+        MarkerData markerComponent = marker.GetComponent<MarkerData>();
+        if (markerComponent != null)
+        {
+            // 기존 컴포넌트의 데이터 업데이트
+            markerComponent.information = markerData["information"].ToString();
+            markerComponent.level = Convert.ToInt32(markerData["level"]);
+            markerComponent.creationTime = ((Timestamp)markerData["creationTime"]).ToDateTime();
+            markerComponent.location = markerData["location"].ToString();
+            // 기타 필요한 데이터 업데이트
+        }
+    }
 }
