47533f719a0bd1d6537c1bbc0931a02ca5b05cf3
Yongseok Lee <83351867+YSLee551@users.noreply.github.com>
2024-02-19 22:40:50 +0900

    [UPDATE] SensorLoader에 ProcessOutputData 옮기기

--- Assets/Scripts/Sensor/SensorLoader.cs
+++ Assets/Scripts/Sensor/SensorLoader.cs
@@ -4,6 +4,7 @@ using Firebase.Extensions;
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using Uduino;
 
 public class SensorLoader : MonoBehaviour
 {
@@ -11,14 +12,17 @@ public class SensorLoader : MonoBehaviour
     private FirebaseFirestore db;
     private CollectionReference sensorDataCollection;
 
+    UduinoDevice outputDevice = null;
+    UduinoManager UduManager;
+
     [SerializeField] private string sensorPackageID;
 
     // Sensor Data
-    public double temperature;
-    public int lightLevel;
-    public int waterLevel;
-    public int flameDetected;
-    public double humanDetected;
+    [SerializeField] private double temperature;
+    [SerializeField] private int lightLevel;
+    [SerializeField] private int waterLevel;
+    [SerializeField] private int flameDetected;
+    [SerializeField] private double humanDetected;
     // public int buttonPressed;
 
     // Start is called before the first frame update
@@ -30,7 +34,9 @@ public class SensorLoader : MonoBehaviour
         // Get a reference to the sensors collection
         db = FirebaseFirestore.DefaultInstance;
         sensorDataCollection = db.Collection("SensorPackages").Document(sensorPackageID).Collection("SensorData");
-        
+
+        UduManager = UduinoManager.Instance;
+
         // Listen for changes in the sensor data
         ListenForSensorData();
     }
@@ -38,7 +44,7 @@ public class SensorLoader : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        outputDevice = UduinoManager.Instance.GetBoard(sensorPackageID + "_out");
     }
 
     // Listen for changes in the sensor data
@@ -59,8 +65,64 @@ public class SensorLoader : MonoBehaviour
                 // buttonPressed = Convert.ToInt32(sensorData["buttonPressed"]);
 
                 Debug.Log("New sensor data received: " + temperature + " " + lightLevel + " " + waterLevel + " " + flameDetected + " " + humanDetected);
+            
+                if (outputDevice != null)
+                {
+                    ProcessOutputData();
+                }
             }
+            
         });
+    }
+
+    void ProcessOutputData()
+    {
+        // Debug.Log("Board2 is connected");
+
+        // [RGB LED]
+        // RGB LED : Pin 9, 10, 11
+        // UduManager.pinMode(outputDevice, 9, PinMode.Output);
+        // UduManager.pinMode(outputDevice, 10, PinMode.Output);
+        // UduManager.pinMode(outputDevice, 11, PinMode.Output);
+
+        // // RGB LED
+        // UduManager.analogWrite(outputDevice, 9, redIntensity);
+        // UduManager.analogWrite(outputDevice, 10, greenIntensity);
+        // UduManager.analogWrite(outputDevice, 11, blueIntensity);
+
+        // [LCD Display]
+        // lightLevel을 3자리 숫자로 변환하여 displayValue에 저장
+        string displayMessage = "DisplayData";
+
+        // Display Light Level
+        displayMessage += " " + "L:";
+        if (lightLevel.ToString().Length < 3)
+        {
+            displayMessage += lightLevel.ToString().PadLeft(3, '0');
+        }
+        else
+        {
+            displayMessage += lightLevel;
+        }
+
+        // Display Temperature
+        displayMessage += " " + "T:";
+        displayMessage += temperature;
+
+        // Display Water Level
+        displayMessage += " " + "W:";
+        if (waterLevel.ToString().Length < 3)
+        {
+            displayMessage += waterLevel.ToString().PadLeft(3, '0');
+        }
+        else
+        {
+            displayMessage += waterLevel;
+        }
+
+        // Debug.Log("Display Value: " + displayMessage);
+
+        UduManager.sendCommand(outputDevice, displayMessage);
     }
 }
 
