2272b2ced6205934febc86b87beecb0333caedec
Yongseok Lee <83351867+YSLee551@users.noreply.github.com>
2024-02-22 16:25:20 +0900

    [UPDATE] 보드 및 핀 구분하여 데이터 송수신 완료

--- Assets/Scripts/Sensor/SensorTest.cs
+++ Assets/Scripts/Sensor/SensorTest.cs
@@ -8,16 +8,16 @@ using Firebase;
 using Firebase.Firestore;
 using Firebase.Extensions;
 
-public class SensorUploader : MonoBehaviour
+public class SensorTest : MonoBehaviour
 {
     private FirebaseFirestore db;
-    private float elapsedTime = 0f;
-
     private UduinoManager UduManager;
     private UduinoDevice inputDevice = null;
     private UduinoDevice outputDevice = null;
 
+    // SensorPackage ID
     [SerializeField] private string sensorPackageID;
+
     enum Pin
     {
         D0 = 0,
@@ -50,13 +50,7 @@ public class SensorUploader : MonoBehaviour
     [SerializeField] private int flameDetected;
     [SerializeField] private double humanDetected;
     [SerializeField] private int buttonPressed;
-
-    [SerializeField] private List<double> temperatureCs = new List<double>();
-    [SerializeField] private List<int> lightLevels = new List<int>();
-    [SerializeField] private List<int> waterLevels = new List<int>();
-    [SerializeField] private List<int> flameDetecteds = new List<int>();
-    [SerializeField] private List<double> humanDetecteds = new List<double>();
-    [SerializeField] private List<int> buttonPresseds = new List<int>();
+    // [SerializeField] private float distance = 0;
 
     // Output
     [Range(0, 255)] public int redIntensity;
@@ -70,23 +64,34 @@ public class SensorUploader : MonoBehaviour
         UduManager = UduinoManager.Instance;
 
         sensorPackageID = this.gameObject.name;
+
+        //UduManager.OnBoardConnected += OnBoardConnected;
+
+        // UduinoManager.Instance.OnDataReceived += DataReceived;
+
+        // // Temperature + Humidity Sensor : Serial
+        // UduManager.OnDataReceived += DataReceived;
     }
+
+    // void DataReceived(string value, UduinoDevice board)
+    // {
+    //     Debug.Log("Value: " + value + " Board: " + board);
+    // }
 
     // Update is called once per frame
     void Update()
     {
-        if (UduinoManager.Instance.hasBoardConnected())
+        if (UduManager.hasBoardConnected())
         {
-            inputDevice = UduinoManager.Instance.GetBoard(sensorPackageID + "_in");
-            outputDevice = UduinoManager.Instance.GetBoard(sensorPackageID + "_out");
+            inputDevice = UduManager.GetBoard(sensorPackageID + "_in");
+            outputDevice = UduManager.GetBoard(sensorPackageID + "_out");
 
-            // Check if the input board is connected and process the input data
+            // Check if the board is connected
             if (inputDevice != null)
             {
                 ProcessInputData();
             }
-             
-            // Check if the output board is connected and process the output data
+
             if (outputDevice != null)
             {
                 ProcessOutputData();
@@ -94,19 +99,23 @@ public class SensorUploader : MonoBehaviour
         }
         else
         {
-            Debug.Log("## NO BOARDS DETECTD ##");
+            Debug.Log("The boards have not been detected");
         }
-
-        if (inputDevice != null && elapsedTime >= 10f)
-        {
-            UploadToDB();
-            elapsedTime = 0f;
-        }
-
-        elapsedTime += Time.deltaTime;
     }
 
-    #region Board Data Processing (Input & Output)
+    // Different setups for each arduino board
+    void OnBoardConnected(UduinoDevice connectedDevice)
+    {
+        //You can launch specific functions here
+        if (connectedDevice.name == sensorPackageID + "_in")
+        {
+            inputDevice = connectedDevice;
+        }
+        else if (connectedDevice.name == sensorPackageID + "_out")
+        {
+            outputDevice = connectedDevice;
+        }
+    }
 
     void ProcessInputData()
     {
@@ -118,11 +127,6 @@ public class SensorUploader : MonoBehaviour
         ReadDataFrom((int)Pin.A3);
         ReadDataFrom((int)Pin.D2);
         ReadDataFrom((int)Pin.D4);
-
-        // Result Log
-        // string resultLog = "Temperature: " + temperatureC + " || Light: " + lightLevel + " || Water: " + waterLevel + " || Flame: " + flameDetected + " || Human: " + humanDetected + " || Button: " + buttonPressed;
-
-        AddDataToLists(temperatureC, lightLevel, waterLevel, flameDetected, humanDetected);
     }
 
     void ProcessOutputData()
@@ -149,7 +153,7 @@ public class SensorUploader : MonoBehaviour
         {
             data = UduManager.Read(inputDevice, "rd " + pin);
         }
-
+       
         // Split the string and convert it to int
         if (!string.IsNullOrEmpty(data))
         {
@@ -255,145 +259,4 @@ public class SensorUploader : MonoBehaviour
 
         UduManager.sendCommand(outputDevice, displayMessage);
     }
-    #endregion
-
-    #region Database Upload (Sensor Data & Alert)
-    private void AddDataToLists(double temperatureC, int lightLevel, int waterLevel, int flameDetected, double humanDetected)
-    {
-        temperatureCs.Add(temperatureC);
-        lightLevels.Add(lightLevel);
-        waterLevels.Add(waterLevel);
-        flameDetecteds.Add(flameDetected);
-        humanDetecteds.Add(humanDetected);
-        return;
-    }
-
-    private void UploadToDB()
-    {
-        Timestamp createdTime = Timestamp.FromDateTime(DateTime.Now);
-
-        Dictionary<string, object> sensorDict = new Dictionary<string, object>
-        {
-            {"createdTime",     createdTime}, // DateTime ???? ???? ????
-            {"temperature",     GetAverage(temperatureCs) },
-            {"lightLevel",      GetAverage(lightLevels) },
-            {"waterLevel",      GetAverage(waterLevels) },
-            {"flameDetected",   GetAverage(flameDetecteds) },
-            {"humanDetected",   GetAverage(humanDetecteds) },
-        };
-
-        // Upload Alert if unusual activity is detected
-        CheckForAlertAndUpload(sensorDict);
-
-        // Upload sensor data to Firestore
-        db.Collection("SensorPackages").Document(sensorPackageID).Collection("SensorData").Document(createdTime.ToDateTime().ToString("yyyy-MM-dd HH:mm:ss")).SetAsync(sensorDict);
-
-        Debug.Log("[Sensor Data Upload] " + sensorPackageID + " " + createdTime.ToDateTime().ToString("yyyy-MM-dd HH:mm:ss") + " " + sensorDict["temperature"] + " " + sensorDict["lightLevel"] + " " + sensorDict["waterLevel"] + " " + sensorDict["flameDetected"] + " " + sensorDict["humanDetected"]);
-
-        ResetLists();
-
-        return;
-    }
-
-    // Check if the sensor data is unusual and upload an alert to Firestore
-    private void CheckForAlertAndUpload(Dictionary<string, object> sensorDict)
-    {
-        string[] parse = sensorPackageID.Split('_');
-
-        bool uploadAlert = false;
-
-        // Alert Dictionary
-        Timestamp createdTime = (Timestamp)sensorDict["createdTime"];
-        Dictionary<string, object> alertDict = new Dictionary<string, object>{
-                {"createdTime", createdTime},
-                {"location", parse[0]},
-                {"sensorPackageNum", parse[1]},
-            };
-
-        string alertLog = "[Alert Upload] ";
-
-        // Check for unusual activity
-        if (Convert.ToDouble(sensorDict["temperature"]) > 30.0)
-        {
-            alertDict.Add("temperature", Convert.ToDouble(sensorDict["temperature"]));
-            uploadAlert = true;
-
-            alertLog += "Temperature: " + sensorDict["temperature"] + " ";
-        }
-
-        if (Convert.ToInt32(sensorDict["lightLevel"]) > 400)
-        {
-            alertDict.Add("lightLevel", Convert.ToInt32(sensorDict["lightLevel"]));
-            uploadAlert = true;
-
-            alertLog += "Light: " + sensorDict["lightLevel"] + " ";
-        }
-
-        if (Convert.ToInt32(sensorDict["waterLevel"]) > 400)
-        {
-            alertDict.Add("waterLevel", Convert.ToInt32(sensorDict["waterLevel"]));
-            uploadAlert = true;
-
-            alertLog += "Water: " + sensorDict["waterLevel"] + " ";
-        }
-
-        if (Convert.ToDouble(sensorDict["flameDetected"]) > 3.0)
-        {
-            alertDict.Add("flameDetected", Convert.ToDouble(sensorDict["flameDetected"]));
-            uploadAlert = true;
-
-            alertLog += "Flame: " + sensorDict["flameDetected"] + " ";
-        }
-
-        if (Convert.ToDouble(sensorDict["humanDetected"]) > 0.5)
-        {
-            alertDict.Add("humanDetected", Convert.ToDouble(sensorDict["humanDetected"]));
-            uploadAlert = true;
-
-            alertLog += "Human: " + sensorDict["humanDetected"] + " ";
-        }
-
-        // Upload the alert to Firestore
-        if (uploadAlert)
-        {
-            Debug.Log(alertLog);
-            db.Collection("IssueAlerts").Document(createdTime.ToDateTime().ToString("yyyy-MM-dd HH:mm:ss")).SetAsync(alertDict);
-        }
-    }
-
-    private double GetAverage(List<double> list)
-    {
-        double result = 0;
-        foreach (var item in list)
-        {
-            result += item;
-        }
-
-        result /= list.Count;
-        result = Math.Round(result * 100) / 100;
-        return result;
-    }
-
-    private int GetAverage(List<int> list)
-    {
-        int result = 0;
-        foreach (var item in list)
-        {
-            result += item;
-        }
-
-        result /= list.Count;
-        return result;
-    }
-
-    private void ResetLists()
-    {
-        temperatureCs.Clear();
-        waterLevels.Clear();
-        lightLevels.Clear();
-        flameDetecteds.Clear();
-        humanDetecteds.Clear();
-        return;
-    }
-    #endregion
 }
