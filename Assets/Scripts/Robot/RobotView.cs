155fc78eb378abe66320659565acad43f03b88b9
hgudrlee <97952121+hgudrlee@users.noreply.github.com>
2024-02-23 14:06:42 +0900

    대시보드 Reload 버튼 제작, 로봇뷰 패널 버튼 연결

--- Assets/Scripts/Robot/RobotView.cs
+++ Assets/Scripts/Robot/RobotView.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using RosSharp.RosBridgeClient;
 using UnityEngine;
 
 public class RobotView : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject rosConnector;
+    public GameObject robotViewPanel;
+    private bool isRobotViewActive = false;
+
     void Start()
     {
-        
+        robotViewPanel.SetActive(false);
+        rosConnector.GetComponent<ImageSubscriber>().enabled = false;
+    }
+    
+    public void OnClickRobotViewButton()
+    {
+        if (!isRobotViewActive)
+        {
+            // ImageSubscriber를 활성화
+            rosConnector.GetComponent<ImageSubscriber>().enabled = true;
+
+            // RobotViewPanel 활성화
+            robotViewPanel.SetActive(true);
+
+            isRobotViewActive = true;
+        }
+        else
+        {
+            CloseRobotView();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void CloseRobotView()
     {
-        
+        // ImageSubscriber를 비활성화
+        rosConnector.GetComponent<ImageSubscriber>().enabled = false;
+
+        // RobotViewPanel 비활성화
+        robotViewPanel.SetActive(false);
+
+        isRobotViewActive = false;
     }
 }
