84c7aca0186c6302958cccd5020bf9297f402178
hgudrlee <97952121+hgudrlee@users.noreply.github.com>
2024-02-26 16:42:26 +0900

    방 둘러보기, 로봇뷰, 리포트 링크 추가 [MainScene]
    방 둘러보기, 로봇뷰, 리포트 링크 추가 [MainScene]

--- Assets/Scripts/Report/WebView.cs
+++ Assets/Scripts/Report/WebView.cs
@@ -1,10 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Report : MonoBehaviour
+public class ReportWebSite : MonoBehaviour
 {
-    public void ReportWebsite()
+    public void GoToWebSite()
     {
         Application.OpenURL("https://lookerstudio.google.com/reporting/ee68fa00-6119-4945-a095-11b6c422ca26/page/tEnnC");
     }
