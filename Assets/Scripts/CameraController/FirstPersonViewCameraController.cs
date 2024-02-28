295d922e7e74bcf4a86cd76fde73239a4f4e9756
hgudrlee <97952121+hgudrlee@users.noreply.github.com>
2024-02-28 11:17:59 +0900

    Top View 버튼 삭제
    Top View 버튼 삭제

--- Assets/Scripts/CameraController/FirstPersonViewCameraController.cs
+++ Assets/Scripts/CameraController/FirstPersonViewCameraController.cs
@@ -9,7 +9,6 @@ public class FirstPersonViewCameraController : MonoBehaviour
     public float rotateSpeed = 100.0f; // 회전 속도 조절 변수
     private Camera attachedCamera; // 현재 스크립트가 부착된 카메라
     private Vector3 prevMousePosition; // 이전 마우스 위치 저장 변수
-    public TMP_Dropdown roomsDropdown;
 
     void Start()
     {
@@ -80,41 +79,5 @@ public class FirstPersonViewCameraController : MonoBehaviour
     {
         // 현재 위치에서 y좌표를 내림
         transform.position -= new Vector3(0, moveSpeed * Time.deltaTime, 0);
-    }
-
-    public void MoveToRoom(string roomName)
-    {
-        Vector3 targetPosition = Vector3.zero;
-        Vector3 targetRotation = Vector3.zero;
-
-        // 방 이름에 따라 목표 위치와 회전값 설정
-        switch (roomName)
-        {
-            case "NTH313":
-                targetPosition = new Vector3(27, -2, -33);
-                targetRotation = new Vector3(0, 218, 0);
-                break;
-            case "NTH311":
-                targetPosition = new Vector3(26, -0.7f, -9);
-                targetRotation = new Vector3(0, 434, 0);
-                break;
-            case "Hallway":
-                targetPosition = new Vector3(18.58f, -0.72f, -16.8f);
-                targetRotation = new Vector3(0, -38.51f, 0);
-                break;
-            case "ANH313":
-                targetPosition = new Vector3(2,-0.9f, 9);
-                targetRotation = new Vector3(0, -176, 0);
-                break;
-            default:
-                Debug.LogError("Unknown room name: " + roomName);
-                return;
-        }
-
-        // 카메라의 위치를 목표 위치로 이동
-        transform.position = targetPosition;
-
-        // 카메라의 회전을 목표 회전으로 설정
-        transform.eulerAngles = targetRotation;
     }
 }
